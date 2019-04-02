726ff744663ccc03a0cba5eb5ac77f76f8927b43
HP <HP@HP>
2019-04-02 22:12:23 +0430

    Solving Some Bugs of Program. And We are still in Frist Version of Program.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,27 @@
 ﻿using System;
 using System.IO;
 using Microsoft.Win32;
-namespace DeletingTempAndPrefetchFiles
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Security.AccessControl;
+namespace DeletingTempAndPrefetchFiles 
 {
     class Program
-    {
-        static void Main(string[] args)
-        {
+    {    
+        static  void Main(string[] args)
+        {           
+          
 			//Console.Title("----Windows Cleaner-----");
             // Set Windows Startup For Program
             Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
             key.SetValue("CleanerTempFiles", @"C:\Users\HP\Documents\tmp\DeletingTempAndPrefetchFiles\bin\Debug\DeletingTempAndPrefetchFiles.exe");
 
             // Call Method with Params
-            DeleteFileAndDirectories(@"C:\Windows\Temp", @"C:\Windows\Prefetch");
+             DeleteFileAndDirectories(@"C:\Windows\Temp", @"C:\Windows\Prefetch");
         }
 
         // Implement Method For Deleting
-        public static void DeleteFileAndDirectories(params string[] address)
+        public  static void  DeleteFileAndDirectories(params string[] address)
         {
             // Select Element in Address Collections
             foreach (var adr in address)
@@ -25,10 +29,12 @@ namespace DeletingTempAndPrefetchFiles
                 DirectoryInfo dic = new DirectoryInfo(adr);
                 Console.ForegroundColor = ConsoleColor.DarkGray;
                 Console.WriteLine($"\n-----------------------------Address:{adr}------------------------------");
+               
                 foreach (var element in dic.GetFiles())
                 {
                     try
                     {
+                        
                         element.Delete();
 
                     }
